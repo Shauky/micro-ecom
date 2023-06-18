342506384c6cab52c4e63295481b610508c1968f
deviant <devi4nt@Shaukaths-MacBook-Pro.local>
2023-06-18 16:57:03 +0500

    test with new db

---
+++ src/cfehome/db.py
@@ -0,0 +1,11 @@
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'neondb',
+    'USER': 'shaukathmohamed',
+    'PASSWORD': '6Hg8jGsaDboL',
+    'HOST': 'ep-misty-flower-640153.ap-southeast-1.aws.neon.tech',
+    'PORT': '5432',
+  }
+}
